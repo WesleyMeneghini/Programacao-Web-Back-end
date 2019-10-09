aa61249b173479f100cc3524e3bb547ff0172e8e
Wesley Meneghini <wesley.meneghini@outlook.com.br>
2019-10-09 14:01:34 -0300

    colocando projeto atualizado no github

---
+++ cms/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style_cms.css">
+        <title>CMS - Gerenciamento</title>
+    </head>
+    <body>
+        <div id="container_cms" class="center">
+            <div id="cabecalho">
+                <div id="titulo_cms">
+                    <h2> CMS - Sistema de Gerenciamento do Site</h2>
+                </div>
+                <div id="logo_empresa">
+                    <figure>
+                        <img src="img/logo_delicia_gelada2.png" class="bkg-img">
+                    </figure>
+                </div>
+            </div>
+            <div id="menu">
+                <nav>
+                    <ul>
+                        <li class="menu_itens txt-center">
+                            <div class="menu_img center">
+                                <figure>
+                                    <img src="icones/bloqueio.png" class="bkg-img">
+                                </figure>
+                            </div>
+                            <p class="negrito">
+                                Adm. Conteúdo
+                            </p>
+                        </li><li class="menu_itens txt-center">
+                            <div class="menu_img center">
+                                <figure>
+                                    <img src="icones/bloqueio.png" class="bkg-img">
+                                </figure>
+                            </div>
+                            <p class="negrito">
+                                Adm. Fale Conosco
+                            </p>
+                        </li><li class="menu_itens txt-center">
+                            <div class="menu_img center">
+                                <figure>
+                                    <img src="icones/bloqueio.png" class="bkg-img">
+                                </figure>
+                            </div>
+                            <p class="negrito">
+                                Adm. Usuários
+                            </p>
+                        </li>
+                    </ul>
+                </nav>
+                <div id="identificacao">
+                    <p>
+                        Bem vindo, (xxxxxxxxxxxxxx).
+                    </p>
+                    <p id="logout">
+                        Logout
+                    </p>
+                
+                </div>
+            </div>
+            <section id="conteudo">
+                <div class="paginas_editaveis">
+                    <div class="icone_imagem">
+                    
+                    </div>
+                </div>
+                <div class="paginas_editaveis">
+                </div>
+                <div class="paginas_editaveis">
+                </div>
+            </section>
+            <footer>
+                <p class="txt-center">
+                    Desenvolvido por Wesley Meneghini
+                </p>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ rodape.php
@@ -0,0 +1,23 @@
+
+<footer>
+    <div class="conteudo center">
+        <div id="container_sistema_interno">
+            <div id="sistema_interno" class="bkg-primary">
+                Sistema Interno
+            </div>
+        </div>
+        <div id="container_endereco">
+            <div class="endereco center">
+                ENDEREÇO: Av. Luis Carlos Berrini, nº 666
+            </div>
+        </div>
+        <div id="icone_download">
+            <figure>
+                <img src="icones/android.png" class="bkg-img" alt="download play story">
+            </figure>
+        </div>
+        <div id="download_app" class="bkg-primary">
+            Baixe o App
+        </div>
+    </div>
+</footer>
